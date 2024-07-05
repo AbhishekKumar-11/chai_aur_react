a87ce66f243ea797428a70458f884324939b3a2e
AbhishekKumar-11 <rishu2152@gmail.com>
2024-07-05 19:25:31 +0530

    how do we start a react project the two ways

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+   <h1>chai aur react | i learnt </h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react" ;
+
+
+
+function App() {
+
+
+  return (
+    <h1>chai aur react with vite | AK</h1>
+  )
+}
+
+export default App;
